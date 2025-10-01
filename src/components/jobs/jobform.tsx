f008ac72866abb0c13abd0afea8ec43c73e1deb8
b.munkhbaaatar10 <b.munkhbaatar10@gmail.com>
2025-10-01 16:00:21 +0800

    up

--- src/components/jobs/jobform.tsx
+++ src/components/jobs/jobform.tsx
@@ -55,7 +55,7 @@ export default function JobForm({ job, preSelectedPosition }: JobFormProps) {
         return;
       }
       if (f.size > maxSize) {
-        setError("Файл 5MB-аас их  болохгүй.");
+        setError("Файл 5MB-аас их байж болохгүй.");
         setFile(null);
         return;
       }
